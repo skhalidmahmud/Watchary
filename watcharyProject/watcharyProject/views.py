8b61b1fc0a294a1c3687084265446a0b0f1d84ff
Khalid Mahmud <skhalidmahmud1@gmail.com>
2025-05-30 23:30:27 +0600

    Add frontend from GPT

---
+++ watcharyProject/watcharyProject/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+
+def home(request):
+    return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+def contact(request):
+    return render(request, 'contact.html')
+def new(request):
+    return render(request, 'new.html')
+def popular(request):
+    return render(request, 'popular.html')
+def search(request):
+    return render(request, 'search.html')
+def sign_in(request):
+    return render(request, 'signin.html')
